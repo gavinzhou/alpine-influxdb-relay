fcaebd41d5e000a94ec649ffdc63aca6bafc4708
Gavin Zhou <gavin.zhou@gmail.com>
2016-07-30 18:20:47 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,19 +11,7 @@ log_msg() {
   echo "[$(date "+%Y/%m/%d %H:%M:%S %z")] $@"
 }
 
-docker_build() {
-  # CircleCI cannot build docker images with --rm=true correctly.
-  if [ -z "$CIRCLE_BUILD_NUM" ]; then
-    docker build --rm=false "$@"
-  else
-    docker build --rm=true "$@"
-  fi
-}
-
 log_msg "Verifying docker daemon connectivity"
-docker version
-
-failed_builds=()
 
 # Gather directories with a Dockerfile and sanitize the path to remove leading
 # a leading ./ and multiple slashes into a single slash.
@@ -31,17 +19,8 @@ dockerfiles=$(find "$dir" -name Dockerfile -print0 | xargs -0 -I{} dirname {} | 
 for path in $dockerfiles; do
   # Generate a tag by replacing the first slash with a colon and all remaining slashes with a dash.
   tag=$(echo $path | sed 's@/@:@' | sed 's@/@-@g')
-  log_msg "Building docker image $tag (from $path)"
+  log_msg "Tagging docker image $tag with gcr tag"
   docker tag "orangesys/alpine-influxdb-relay:${tag}" "asia.gcr.io/orange-sys/alpine-influxdb-relay:${tag}"
-  gcloud docker push asia.gcr.io/orange-sys/alpine-influxdb-relay:${tag}
+  sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/orange-sys/alpine-influxdb-relay:${tag}
 
 done
-
-if [ ${#failed_builds[@]} -eq 0 ]; then
-  log_msg "All builds succeeded."
-else
-  log_msg "Failed to build the following images:"
-  for tag in ${failed_builds[@]}; do
-    echo "	$tag"
-  done
-fi
